4e66c6788f6aa39c190f7ec14803c23754c81810
chaiweixuan <573271137@qq.com>
2022-06-27 00:23:27 +0800

    init commit

---
+++ Assets/DeepMapTmx/note_d.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="note_d" tilewidth="160" tileheight="116" tilecount="1" columns="1">
+ <image source="images/note/note_d.png" width="160" height="116"/>
+</tileset>
---
+++ Assets/MeshTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshTest : MonoBehaviour
+{
+    public Material mat;
+    
+    private MeshRenderer _meshRenderer;
+    private MeshFilter _meshFilter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        _meshFilter = gameObject.AddComponent<MeshFilter>();
+        StartDrawQuad();
+    }
+
+    private void StartDrawQuad()
+    {
+        _meshRenderer.material = mat;
+        
+        List<Vector3> verticesList = new List<Vector3>();
+        verticesList.Add(new Vector3(0, 0, 0));
+        verticesList.Add(new Vector3(0, 0, 1));
+        verticesList.Add(new Vector3(1, 0, 1));
+        verticesList.Add(new Vector3(1, 0, 0));
+
+        List<int> triIndexList = new List<int>();
+        triIndexList.Add(0);
+        triIndexList.Add(1);
+        triIndexList.Add(2);
+        triIndexList.Add(2);
+        triIndexList.Add(3);
+        triIndexList.Add(0);
+
+        List<Vector2> uvList = new List<Vector2>();
+        uvList.Add(new Vector2(0, 0));
+        uvList.Add(new Vector2(0,0.5f));
+        uvList.Add(new Vector2(0.5f,0.5f));
+        uvList.Add(new Vector2(0.5f,0));
+
+        List<Color> verticesColorList = new List<Color>();
+        verticesColorList.Add(Color.red);
+        verticesColorList.Add(Color.green);
+        verticesColorList.Add(Color.blue);
+        verticesColorList.Add(Color.white);
+
+
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = verticesList.ToArray();
+        mesh.triangles = triIndexList.ToArray();
+        mesh.uv = uvList.ToArray();
+        mesh.colors = verticesColorList.ToArray();
+
+        _meshFilter.mesh = mesh;
+    }
+}
